fae9650a76be08069b5140a911710f68aaa5e621
kim (grufwub) <grufwub@gmail.com>
2019-08-14 13:23:25 +0000

    add dap_logging init + close to main.py
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
-import get_cases
-import screen_cases
-import identify_cases
-import send_letters
+from dap_logging import dap_logging_init, dap_logging_close
+
+try:
+    dap_logging_init()
+    import get_cases
+    import screen_cases
+    import identify_cases
+    import send_letters
+finally:
+    dap_logging_close()
