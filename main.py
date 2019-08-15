33014b429fc957081d318908ace7e35bf8478bb0
kim (grufwub) <grufwub@gmail.com>
2019-08-15 15:32:10 +0000

    add code to main.py to get DAP-breaking exceptions and log as critical to general
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from dap_logging import dap_logging_init, dap_logging_close
+from dap_logging import dap_logging_init, dap_logging_close, dap_log_general
 
 try:
     dap_logging_init(debug = True)
@@ -8,5 +8,7 @@ try:
     import screen_cases
     import identify_cases
     import send_letters
+except Exception as e:
+    dap_log_general(LogLevel.CRITICAL, "exception encountered [%i]: %s" % (e.errno, e.strerror))
 finally:
     dap_logging_close()
