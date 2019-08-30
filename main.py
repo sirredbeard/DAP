3318b13d3c4dce47db0ec13459a1f2a2861c583a
kim (grufwub) <grufwub@gmail.com>
2019-08-30 12:10:33 +0000

    fix error handling in main.py + add traceback to log
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-from dap_logging import dap_logging_init, dap_logging_close, dap_log_general
+import traceback
+from dap_logging import dap_logging_init, dap_logging_close, dap_log_general, LogLevel
 
 try:
     dap_logging_init(debug = True)
@@ -8,7 +9,7 @@ try:
     import screen_cases
     import identify_cases
     import send_letters
-except Exception as e:
-    dap_log_general(LogLevel.CRITICAL, "exception encountered [%i]: %s" % (e.errno, e.strerror))
+except Exception as err:
+    dap_log_general(LogLevel.CRITICAL, "exception encountered: %s\n%s" % (err, traceback.format_exc()))
 finally:
     dap_logging_close()
